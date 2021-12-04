3384d248704e981f46bee7cd266b56b812a67bda
Takatoshi-Miura <takatoshi052031@gmail.com>
2021-12-04 10:45:16 +0900

    [Add] ノート追加画面の課題セルを追加

--- ProcessNote/Controller/Note/AddNoteViewController.swift
+++ ProcessNote/Controller/Note/AddNoteViewController.swift
@@ -14,10 +14,17 @@ class AddNoteViewController: UIViewController {
     @IBOutlet weak var naviItem: UINavigationItem!
     @IBOutlet weak var tableView: UITableView!
     var sectionTitle: [String] = []
-    var realmGroupArray: [Group] = []
+    var groupArray: [Group] = []
+    var taskArray: [Task] = []
     var pickerView = UIView()
     let colorPicker = UIPickerView()
     var pickerIndex: Int = 0
+    
+    enum Section: Int {
+        case group = 0
+        case task
+        case save
+    }
     
     // MARK: LifeCycle
     override func viewDidLoad() {
@@ -25,7 +32,8 @@ class AddNoteViewController: UIViewController {
         initNavigationBar()
         initTableView()
         initColorPicker()
-        realmGroupArray = getGroupArrayForTaskView()
+        groupArray = getGroupArrayForTaskView()
+        taskArray = getTasksInGroup(ID: groupArray[pickerIndex].getGroupID(), isCompleted: false)
     }
     
     func initNavigationBar() {
@@ -33,9 +41,9 @@ class AddNoteViewController: UIViewController {
     }
     
     func initTableView() {
-        sectionTitle = [NSLocalizedString("Group", comment: "")]
+        sectionTitle = [NSLocalizedString("Group", comment: ""), NSLocalizedString("Task", comment: ""), ""]
         tableView.register(UINib(nibName: "ColorCell", bundle: nil), forCellReuseIdentifier: "ColorCell")
-        tableView.register(UINib(nibName: "TextViewCell", bundle: nil), forCellReuseIdentifier: "TextViewCell")
+        tableView.register(UINib(nibName: "NoteTextViewCell", bundle: nil), forCellReuseIdentifier: "NoteTextViewCell")
         tableView.register(UINib(nibName: "SaveButtonCell", bundle: nil), forCellReuseIdentifier: "SaveButtonCell")
         if #available(iOS 15.0, *) {
             tableView.sectionHeaderTopPadding = 0
@@ -56,6 +64,7 @@ class AddNoteViewController: UIViewController {
         // 選択したIndexを取得して閉じる
         pickerIndex = colorPicker.selectedRow(inComponent: 0)
         closePicker(pickerView)
+        taskArray = getTasksInGroup(ID: groupArray[pickerIndex].getGroupID(), isCompleted: false)
         tableView.reloadData()
     }
     
@@ -83,18 +92,59 @@ extension AddNoteViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 1 {
+            if taskArray.isEmpty {
+                return 0
+            } else {
+                return taskArray.count
+            }
+            
+        }
         return 1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ColorCell", for: indexPath) as! ColorCell
-        cell.delegate = self
-        let colorNum = realmGroupArray[pickerIndex].getColor()
-        cell.setColor(colorNum)
-        cell.setTitle(realmGroupArray[pickerIndex].getTitle())
-        cell.selectionStyle = UITableViewCell.SelectionStyle.none
-        cell.accessibilityIdentifier = "AddNoteViewCell"
-        return cell
+        switch Section(rawValue: indexPath.section) {
+        case .group:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "ColorCell", for: indexPath) as! ColorCell
+            cell.delegate = self
+            let colorNum = groupArray[pickerIndex].getColor()
+            cell.setColor(colorNum)
+            cell.setTitle(groupArray[pickerIndex].getTitle())
+            cell.selectionStyle = UITableViewCell.SelectionStyle.none
+            cell.accessibilityIdentifier = "AddNoteViewCell"
+            return cell
+        case .task:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "NoteTextViewCell", for: indexPath) as! NoteTextViewCell
+            cell.memo.delegate = self
+            cell.setLabelText(taskArray[indexPath.row])
+            cell.memo.inputAccessoryView = createToolBar(#selector(completeAction))
+            cell.selectionStyle = UITableViewCell.SelectionStyle.none
+            cell.accessibilityIdentifier = "AddNoteViewCell"
+            return cell
+        case .save:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "SaveButtonCell", for: indexPath) as! SaveButtonCell
+            cell.delegate = self
+            cell.selectionStyle = UITableViewCell.SelectionStyle.none
+            cell.accessibilityIdentifier = "AddNoteViewCell"
+            return cell
+        default:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+            return cell
+        }
+    }
+    
+    @objc func completeAction() {
+        // キーボードを閉じる
+        self.view.endEditing(true)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.section == Section.task.rawValue {
+            return 158
+        } else {
+            return 44
+        }
     }
     
 }
@@ -124,11 +174,28 @@ extension AddNoteViewController: UIPickerViewDelegate, UIPickerViewDataSource {
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return realmGroupArray.count  // グループ数
+        return groupArray.count  // グループ数
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return realmGroupArray[row].getTitle()   // グループ名
+        return groupArray[row].getTitle()   // グループ名
     }
     
 }
+
+
+extension AddNoteViewController: SaveButtonCellDelegate {
+    
+    func tapSaveButton() {
+        // 入力チェック
+        
+        // 課題データを作成＆保存
+        
+    }
+    
+    func tapCancelButton() {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
+
---
+++ ProcessNote/View/CustomCell/NoteTextViewCell.swift
@@ -0,0 +1,42 @@
+//
+//  NoteTextViewCell.swift
+//  ProcessNote
+//
+//  Created by Takatoshi Miura on 2021/12/04.
+//
+
+import UIKit
+
+class NoteTextViewCell: UITableViewCell, UITextViewDelegate {
+    
+    // MARK: - UI,Variable
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var measures: UILabel!
+    @IBOutlet weak var memo: UITextView!
+    
+    // MARK: - LifeCycle
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        memo.text = ""
+        // 枠線を追加
+        memo.layer.borderWidth = 0.5
+        memo.layer.borderColor = UIColor.black.cgColor
+        memo.layer.cornerRadius = 5.0
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    /**
+     ラベルに文字列をセット
+     - Parameters:
+        - task: 課題
+     */
+    func setLabelText(_ task: Task) {
+        title.text = task.getTitle()
+        let measure = getMeasuresInTask(ID: task.getTaskID()).first!
+        measures.text = "\(NSLocalizedString("Measures", comment: ""))：\(measure.getTitle())"
+    }
+    
+}
